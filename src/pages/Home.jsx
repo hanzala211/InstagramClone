e925b6141648edcaff1f3b1816e5f1ae9f066b89
hanzala211 <hanzalaoc211@gmail.com>
2024-11-22 15:10:39 +0500

    changes again

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -238,7 +238,7 @@ export function Home() {
     return <><section className="w-full max-w-[40%] mx-auto">
         <div className={`flex flex-col gap-2  w-full ${isPostsLoading || homePosts.length === 0 ? "h-[90vh]" : ""} `}>
             {!isPostsLoading ?
-                <InfiniteScroll dataLength={homePosts.length} loader={<Loader height="h-[10vh]" />} next={fetchHomePosts} hasMore={count < 10} >
+                <InfiniteScroll dataLength={homePosts.length} loader={homePosts.length > 0 && <Loader height="h-[10vh]" />} next={fetchHomePosts} hasMore={count < 10} >
                     {
                         homePosts.length > 0 ? homePosts.map((item, i) => {
                             return <div key={i} className="flex flex-col gap-2 mt-7 border-b-[2px] border-[#262626] pb-4">
@@ -295,13 +295,17 @@ export function Home() {
                                 </div>
                             </div>
                         })
-                            : <div className="absolute top-20 left-[52%] flex flex-col items-center gap-1">
-                                <RiUserFollowFill className="text-[40px]" />
-                                <h2 className="text-[40px] font-semibold">Follow Someone</h2>
-                            </div>}
+                            : ''}
 
                 </InfiniteScroll>
                 : <Loader />}
+            {
+                homePosts.length === 0 && !isPostsLoading &&
+                <div className="absolute top-20 left-[52%] flex flex-col items-center gap-1">
+                    <RiUserFollowFill className="text-[40px]" />
+                    <h2 className="text-[40px] font-semibold">Follow Someone</h2>
+                </div>
+            }
         </div>
     </section>
         <Post isPostOpen={isPostOpen} setIsPostOpen={setIsPostOpen} postData={homePosts[currentPost]?.user} page={page} setPage={setPage} currentIndex={currentIndex} setCurrentIndex={setCurrentIndex} currentPost={currentPost} setCurrentPost={setCurrentPost} totalPages={totalPages} setTotalPages={setTotalPages} comments={comments} setComments={setComments} />
