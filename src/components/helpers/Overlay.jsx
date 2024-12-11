e7cd3bc98168ff1de8bfa2f0ec4c99eb062199b0
hanzala211 <hanzalaoc211@gmail.com>
2024-12-11 12:50:57 +0500

    check

--- src/components/helpers/Overlay.jsx
+++ src/components/helpers/Overlay.jsx
@@ -3,7 +3,7 @@ import { IoCloseSharp } from "react-icons/io5";
 export function Overlay({ handleClose, isPostOpen }) {
     return <>
         <IoCloseSharp
-            className={`fixed xl:text-[35px] text-[25px] xl:top-8 lg:top-5 top-2 xl:right-7 lg:right-5 right-2 z-[100000] cursor-pointer opacity-0 ${isPostOpen ? "opacity-100" : "pointer-events-none"}`}
+            className={`fixed xl:text-[35px] text-[25px] xl:top-8 lg:top-5 top-12 xl:right-7 lg:right-5 right-2 z-[100000] cursor-pointer opacity-0 ${isPostOpen ? "opacity-100" : "pointer-events-none"}`}
             onClick={handleClose}
         />
         <div
