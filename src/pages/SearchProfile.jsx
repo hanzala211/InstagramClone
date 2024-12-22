01c23899d9fca8cea1ed98c1b0f5d63af3ef8faa
hanzala211 <hanzalaoc211@gmail.com>
2024-12-22 12:38:00 +0500

    more fixed

--- src/pages/SearchProfile.jsx
+++ src/pages/SearchProfile.jsx
@@ -66,9 +66,9 @@ export function SearchProfile() {
                             <NoteDiv notes={searchUserNotes[0]} />
                         }
                         <Link to={searchUserStatus.length > 0 ? `/search/stories/${selectedProfile.userName}/${searchUserStatus[0]._id}/` : ""}
-                            className={`p-2 ${searchUserStatus.length > 0 ? "relative rounded-full multicolor-border h-[5.9rem] sm:h-32 md:h-auto" : ""}`}
+                            className={`p-2 ${searchUserStatus.length > 0 ? "relative rounded-full multicolor-border h-[5.9rem] sm:h-32 md:h-auto" : ""} flex justify-center items-center`}
                             onClick={() => setCurrentStory(0)}>
-                            <img src={selectedProfile.profilePic} alt="User Profile" className="rounded-full 440:w-[5.4rem] w-[6.6rem] sm:w-28 lg:w-40 md:min-w-[7rem] min-w-[3rem]" />
+                            <img src={selectedProfile.profilePic} alt="User Profile" className="rounded-full w-[5rem] sm:w-28 lg:w-40 lg:min-w-[7rem] min-w-[3rem]  sm:h-28 lg:h-40 object-cover" />
                         </Link>
                         <div className="flex flex-col gap-2 sm:gap-5 mt-2 xl:mt-0">
                             <div className="flex md:flex-row flex-col gap-3 sm:gap-6 md:items-center">
