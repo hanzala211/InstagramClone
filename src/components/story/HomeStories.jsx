ad6cb2148014980fb1828bdcb750662cc23448d1
hanzala211 <hanzalaoc211@gmail.com>
2024-12-22 09:56:06 +0500

    fixes

--- src/components/story/HomeStories.jsx
+++ src/components/story/HomeStories.jsx
@@ -12,13 +12,13 @@ export function HomeStories() {
             <CarouselContent className="flex md:gap-5 gap-3">
                 {homeStories.map((item, index) => (
                     <div key={index} className="flex flex-col items-center gap-0.5">
-                        <Link to={`/search/stories/${item.user.userName}/${item.user.stories[0]._id}/`} className="block rounded-full relative w-16 multicolor-border"
+                        <Link to={`/search/stories/${item.user.userName}/${item.user.stories[0]._id}/`} className="block rounded-full relative w-20 multicolor-border"
                             onClick={() => {
                                 setSearchUserStatus(item.user.stories)
                                 setSelectedProfile(item.user)
                             }}
                         >
-                            <img src={item.user.profilePic} className="rounded-full w-16 h-16 p-1.5" alt="Stories Picture" />
+                            <img src={item.user.profilePic} className="rounded-full w-20 h-20 p-1.5" alt="Stories Picture" />
                         </Link>
                         <p className="text-[#A8A8A8] text-[13px] text-center font-semibold">{item.user.fullName}</p>
                     </div>
