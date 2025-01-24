5bba0c7796aaef6cfbcd28194a09aaf69647f0a7
hanzala211 <hanzalaoc211@gmail.com>
2025-01-24 10:51:54 +0500

    fixes

--- src/services/archive.ts
+++ src/services/archive.ts
@@ -19,27 +19,3 @@ export const getArchives = async (data: any) => {
 		console.error(error)
 	}
 }
-
-export async function fetchArchive(setLoadingArchives: (value: boolean) => void, userData: User, setArchives: (value: ProfileStories[]) => void): Promise<void> {
-	try {
-		setLoadingArchives(true);
-		const response = await fetch(
-			`${import.meta.env.VITE_APP_URL}api/v1/archives`,
-			{
-				method: 'GET',
-				headers: {
-					Authorization: `${userData.data.token}`,
-				},
-				redirect: 'follow',
-			}
-		);
-		const result = await response.json();
-		if (result.message !== 'No archives found.') {
-			setArchives(result.archives);
-		}
-	} catch (error) {
-		console.error(error);
-	} finally {
-		setLoadingArchives(false);
-	}
-}
